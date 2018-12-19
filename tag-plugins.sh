24f351f0814215ed4334f0813e23cd453437ed71
Luiza Pagliari <lpagliari@gmail.com>
2018-12-19 14:09:38 -0200

    [feat] Allow tags to be updated

--- tag-plugins.sh
+++ tag-plugins.sh
@@ -1,10 +1,11 @@
 for dir in * ; do
   if [[ -d $dir  ]] &&  [[ -d $dir/.git ]]; then
     (
-      cd $dir
-      # git push --delete origin 1.0.0-alpha
-      git tag 1.0.0-alpha.2018.02
-      git push origin 1.0.0-alpha.2018.02
+      cd $dir &&
+        # git tag --delete 1.0.0-alpha.2018.11 || true &&
+        git tag 1.0.0-alpha.2018.11 &&
+        # git push --delete origin 1.0.0-alpha.2018.11 || true &&
+        git push origin 1.0.0-alpha.2018.11
     );
   fi
 done
