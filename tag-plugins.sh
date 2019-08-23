e4d7890de6df9762af1f74388d57a37df59760c4
Luiza Pagliari <lpagliari@gmail.com>
2019-08-23 16:08:55 -0300

    Extract tag name into a var, so it can be easily changed

--- tag-plugins.sh
+++ tag-plugins.sh
@@ -1,11 +1,17 @@
+TAG="1.0.0-alpha.2019.07.1"
 for dir in * ; do
   if [[ -d $dir  ]] &&  [[ -d $dir/.git ]]; then
     (
       cd $dir &&
-        # git tag --delete 1.0.0-alpha.2018.11 || true &&
-        git tag 1.0.0-alpha.2018.11 &&
-        # git push --delete origin 1.0.0-alpha.2018.11 || true &&
-        git push origin 1.0.0-alpha.2018.11
+        # Delete any pre-existing tag with the same name.
+        # Only use this if you're replacing an existing tag
+        # git tag --delete $TAG || true &&
+        # git push --delete origin $TAG || true &&
+        git tag $TAG &&
+        git push origin $TAG
     );
   fi
 done
+
+# demais repos:
+# git tag $TAG && git push origin $TAG
